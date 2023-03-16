b5fd9ce8cb2a2b8481969097b0d362c4c2154a42
ShivaniAnandan <124684724+ShivaniAnandan@users.noreply.github.com>
2023-03-16 20:12:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your code here
+let str = "";
+let touristSpots = ['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
+for(let i = 0; i < 3; i++){
+	str += `<li>${touristSpots[i]}</li>`
+}
 
+document.getElementById("band").innerHTML = str;
