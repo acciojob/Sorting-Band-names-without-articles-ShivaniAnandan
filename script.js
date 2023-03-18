91366878ab99b0f86c37a9300051889b523fc6bd
ShivaniAnandan <124684724+ShivaniAnandan@users.noreply.github.com>
2023-03-18 18:50:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 //your code here
-let str = "";
-let touristSpots = ['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
-for(let i = 0; i < 3; i++){
-	str += `<li>${touristSpots[i]}</li>`
-}
+	const bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'The Who', 'Aerosmith', 'Rolling Stones', 'Queen', 'Anthrax', 'Black Sabbath'];
 
-document.getElementById("band").innerHTML = str;
+		function sortBands(bands) {
+			const articles = ['the', 'a', 'an'];
+			const sortedBands = bands.sort(function(a, b) {
+				const nameA = a.toLowerCase().replace(/^(the|an|a)\s+/, '');
+				const nameB = b.toLowerCase().replace(/^(the|an|a)\s+/, '');
+				if (nameA < nameB) {
+					return -1;
+				}
+				if (nameA > nameB) {
+					return 1;
+				}
+				return 0;
+			});
+			return sortedBands;
+		}
+
+		const sortedBands = sortBands(bandNames);
+
+		const bandList = document.getElementById("band");
+		sortedBands.forEach(function(band) {
+			const li = document.createElement("li");
+			li.appendChild(document.createTextNode(band));
+			bandList.appendChild(li);
+		});
+
